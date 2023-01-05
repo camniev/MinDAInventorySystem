c117f719c5946f193ba919ff8b874e4eb73f9155
camniev <camnieve@gmail.com>
2023-01-05 15:15:20 +0800

    Change font from Calibri to Poppins (Google Fonts)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+
+<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
   <style>
   body {
-          font-family: Calibri;
+          font-family: 'Poppins', Arial, sans-serif;
           background-repeat: no-repeat;
           font-size: 12px !important;
           height: 100%;
